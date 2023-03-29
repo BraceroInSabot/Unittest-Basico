6515454eb7535ae7fe44af39f3b706d597a9b589
Guilherme Bracero <guibragon@gmail.com>
2023-03-29 19:26:23 -0300

    Teste com funcao carro (bool)

--- _test.py
+++ _test.py
@@ -1,4 +1,4 @@
-from main import ligar_lampada, porcentagem
+from main import ligar_lampada, porcentagem, carro
 import unittest
 
 
@@ -8,6 +8,9 @@ class Testes(unittest.TestCase):
 
     def teste_porcentagem(self):
         self.assertEqual(porcentagem(num_max=3381, num=29), 980.49)
+
+    def teste_carro(self):
+        self.assertFalse(carro(motor=False, trancado=False))
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def ligar_lampada(condicao: int) -> str:
         return "Luz Acesa"
 
 
-def porcentagem(num_max: float, num: float):
+def porcentagem(num_max: float, num: float) -> float:
     """
     Retorna a o valor da porcentagem de num% de num_max (100%)
 
@@ -30,6 +30,32 @@ def porcentagem(num_max: float, num: float):
     return float("{:.2f}".format(num))
 
 
+def carro(rodas: int = 4, motor: bool = False, trancado: bool = True) -> bool:
+    """
+    Simula uma pessoa ao entrar em um carro, ele precisa estar destrancado e com o motor ligado para dar partida.
+
+    :param:
+    rodas: int -> quantas rodas o carro possui
+    motor: bool -> estado do motor, ligado ou desligado
+    trancado: bool -> estado das fechaduras do carro, trancadas ou destrancadas
+    """
+
+    if rodas >= 4:
+        if not trancado and motor:
+            print("vrum vrum")
+            return True
+        elif trancado and motor:
+            print("precisa destrancar")
+            return False
+        elif trancado or motor:
+            print("onde estão as chaves? na ignição ou no seu bolso?")
+            return False
+
+    print("O carro precisa das suas rodas para andar")
+    return False
+
+
 if __name__ == "__main__":
     ligar_lampada(1)
     porcentagem(num_max=100, num=10)
+    carro(motor=True, trancado=False)
